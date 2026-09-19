9508a1906caba5c4807b63561273aff35b9bb82d
Lucas Silveira <lucasfsil@proton.me>
2026-09-19 05:49:21 -0300

    feat(targets): x86_32 linux and both for windows are added

--- lib/arena.c
+++ lib/arena.c
@@ -1,21 +1,33 @@
 #include "arena.h"
-#include "syscall.h"
 #include <stddef.h>
 
-#define PROT_READ   0x1
-#define PROT_WRITE  0x2
+#if defined(_WIN32)
+#define ARENA_OS_WINDOWS 1
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#elif defined(__linux__)
+#define ARENA_OS_LINUX 1
+#include "syscall.h"
+#else
+#error "arena: os not supported"
+#endif
+
+#if defined(ARENA_OS_LINUX)
+
+#define PROT_READ     0x1
+#define PROT_WRITE    0x2
 #define MAP_PRIVATE   0x02
 #define MAP_ANONYMOUS 0x20
+
+#if defined(__x86_64__)
 
 static inline long
 mangled (mclibcsys6) (
     long number,
-    long a1,
-    long a2,
-    long a3,
-    long a4,
-    long a5,
-    long a6
+    long a1, long a2, long a3,
+    long a4, long a5, long a6
 ) {
     register long r10 __asm__("r10") = a4;
     register long r8  __asm__("r8")  = a5;
@@ -36,27 +48,87 @@ mangled (mclibcsys6) (
     return result;
 }
 
-static inline mangled(addr)
-mangled(mmap) (mangled(addr) ptr, size_t length)
-{   long r = mangled(mclibcsys6)(
-        9, (long)ptr, (long)length,
+static inline long
+mangled(sys_mmap) (size_t length)
+{   return mangled(mclibcsys6)(
+        9, 0, (long)length,
         PROT_READ | PROT_WRITE,
         MAP_PRIVATE | MAP_ANONYMOUS,
         -1, 0
     );
+}
+
+static inline long
+mangled(sys_munmap) (mangled(addr) ptr, size_t length)
+{   return mangled(mclibcsys6)(11, (long)ptr, (long)length, 0, 0, 0, 0);
+}
+
+#elif defined(__i386__)
+
+static inline long
+mangled (mclibcsys3) (long number, long a1, long a2, long a3)
+{   long result;
+    __asm__ volatile (
+        "pushl %%ebx\n\t"
+        "movl  %2, %%ebx\n\t"
+        "int   $0x80\n\t"
+        "popl  %%ebx"
+        : "=a"(result)
+        : "0"(number), "ri"(a1), "c"(a2), "d"(a3)
+        : "memory", "cc"
+    );
+    return result;
+}
+
+static inline long
+mangled(sys_mmap) (size_t length)
+{   struct {
+        unsigned long addr, len, prot, flags, fd, offset;
+    } args = {
+        0, (unsigned long)length,
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        (unsigned long)-1, 0
+    };
+    return mangled(mclibcsys3)(90, (long)&args, 0, 0);
+}
+
+static inline long
+mangled(sys_munmap) (mangled(addr) ptr, size_t length)
+{   return mangled(mclibcsys3)(91, (long)ptr, (long)length, 0);
+}
+
+#else
+#error "arena: Arch Linux not supported"
+#endif
+
+static inline mangled(addr)
+mangled(mmap) (mangled(addr) ptr, size_t length)
+{   (void)ptr;
+    long r = mangled(sys_mmap)(length);
     if( ((unsigned long)r) > (unsigned long)-4096L ) return NULL;
     return (mangled(addr)) r;
 }
 
 static inline int
 mangled(munmap) (mangled(addr) ptr, size_t length)
-{   return (int) mangled(mclibcsys6)(
-        11,
-        (long)ptr,
-        (long)length,
-        0, 0, 0, 0
-    );
+{   return (int) mangled(sys_munmap)(ptr, length);
 }
+
+#elif defined(ARENA_OS_WINDOWS)
+
+static inline mangled(addr)
+mangled(mmap) (mangled(addr) ptr, size_t length)
+{   return VirtualAlloc(ptr, length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+}
+
+static inline int
+mangled(munmap) (mangled(addr) ptr, size_t length)
+{   (void)length;
+    return VirtualFree(ptr, 0, MEM_RELEASE) ? 0 : -1;
+}
+
+#endif
 
 static inline size_t
 mangled(limit) (mangled(t) *a)
